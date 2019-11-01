77d0494c427c7eda97b0fca055dde8751fbd38a3
rayangler <rangetzu8808@gmail.com>
2019-11-01 12:52:54 -0400

    Reformat files

--- src/preview.ts
+++ src/preview.ts
@@ -1,62 +1,80 @@
 /* This file contains functions relevant for Snooty Preview */
 
-import * as vscode from 'vscode';
-import { LanguageClient } from 'vscode-languageclient';
+import * as vscode from "vscode";
+import { LanguageClient } from "vscode-languageclient";
 import { startWebview } from "./webview";
 
 // Registers Snooty Preview as a usable command
-export function registerSnootyPreview(client: LanguageClient, context: vscode.ExtensionContext): void {
-	const snootyPreview: vscode.Disposable = vscode.commands.registerCommand('snooty.snootyPreview', async () => {
-		const projectName: string = await getProjectName(client);
-		const previewPage: string = await getPageFileId(client);
-	
-		// Create task for Snooty Preview
-		const previewBundleTask: vscode.Task = createPreviewBundleTask(context, previewPage, projectName);
-		vscode.tasks.onDidEndTask((e) => {
-				if (e.execution.task === previewBundleTask) {
-						startWebview(context, projectName, previewPage);
-				}
-		});
-	
-		await vscode.commands.executeCommand('snooty.getPageAST');
-		await vscode.tasks.executeTask(previewBundleTask);
-	});
+export function registerSnootyPreview(
+  client: LanguageClient,
+  context: vscode.ExtensionContext
+): void {
+  const snootyPreview: vscode.Disposable = vscode.commands.registerCommand(
+    "snooty.snootyPreview",
+    async () => {
+      const projectName: string = await getProjectName(client);
+      const previewPage: string = await getPageFileId(client);
 
-	context.subscriptions.push(snootyPreview);
+      // Create task for Snooty Preview
+      const previewBundleTask: vscode.Task = createPreviewBundleTask(
+        context,
+        previewPage,
+        projectName
+      );
+      vscode.tasks.onDidEndTask(e => {
+        6;
+        if (e.execution.task === previewBundleTask) {
+          startWebview(context, projectName, previewPage);
+        }
+      });
+
+      await vscode.commands.executeCommand("snooty.getPageAST");
+      await vscode.tasks.executeTask(previewBundleTask);
+    }
+  );
+
+  context.subscriptions.push(snootyPreview);
 }
 
 // Get the project name of the current repo
 async function getProjectName(client: LanguageClient): Promise<string> {
-	return await client.sendRequest("textDocument/get_project_name", {});
+  return await client.sendRequest("textDocument/get_project_name", {});
 }
 
 // Get the file id of the file currently using Snooty Preview
 async function getPageFileId(client: LanguageClient): Promise<string> {
-	const textDocument: vscode.TextDocument = vscode.window.activeTextEditor.document;
-	const fileName: string = textDocument.fileName;
-	return await client.sendRequest("textDocument/get_page_fileid", {filePath: fileName});
+  const textDocument: vscode.TextDocument =
+    vscode.window.activeTextEditor.document;
+  const fileName: string = textDocument.fileName;
+  return await client.sendRequest("textDocument/get_page_fileid", {
+    filePath: fileName
+  });
 }
 
 // Create task to run webpack on snooty frontend via npm run preview
-function createPreviewBundleTask(context: vscode.ExtensionContext, previewPage: string, projectName: string): vscode.Task {
-	const envPreviewPage = `--env.PREVIEW_PAGE='${previewPage}'`;
-	const envProjectName = `--env.PROJECT_NAME='${projectName}'`;
-	const task: vscode.Task = new vscode.Task(
-			{type: 'previewProvider'},
-			vscode.TaskScope.Workspace,
-			"Snooty Preview: Webpack Bundle",
-			"snooty"
-	);
+function createPreviewBundleTask(
+  context: vscode.ExtensionContext,
+  previewPage: string,
+  projectName: string
+): vscode.Task {
+  const envPreviewPage = `--env.PREVIEW_PAGE='${previewPage}'`;
+  const envProjectName = `--env.PROJECT_NAME='${projectName}'`;
+  const task: vscode.Task = new vscode.Task(
+    { type: "previewProvider" },
+    vscode.TaskScope.Workspace,
+    "Snooty Preview: Webpack Bundle",
+    "snooty"
+  );
 
-	// For testing purposes, we want to see the terminal output
-	task.presentationOptions = {
-			reveal: vscode.TaskRevealKind.Always,
-			panel: vscode.TaskPanelKind.New
-	};
-	task.execution = new vscode.ShellExecution(
-			`npm run preview -- ${envPreviewPage} ${envProjectName}`,
-			{ cwd: context.extensionPath }
-	);
+  // For testing purposes, we want to see the terminal output
+  task.presentationOptions = {
+    reveal: vscode.TaskRevealKind.Always,
+    panel: vscode.TaskPanelKind.New
+  };
+  task.execution = new vscode.ShellExecution(
+    `npm run preview -- ${envPreviewPage} ${envProjectName}`,
+    { cwd: context.extensionPath }
+  );
 
-	return task;
+  return task;
 }
--- src/webview.ts
+++ src/webview.ts
@@ -1,98 +1,102 @@
 import * as vscode from "vscode";
-import * as path from 'path';
+import * as path from "path";
 
-export function startWebview(context: vscode.ExtensionContext, projectName: string, previewPage: string) {
-		// Choose template. Guarantees the correct fonts are loaded in as well
-		const template = projectName === 'guides' ? 'guides.css' : 'mongodb-docs.css';
-		// Create panel
-		const panel = vscode.window.createWebviewPanel(
-				'snootyPreview', 
-				`Snooty Preview - ${previewPage}`,
-				{
-					preserveFocus: true,
-					viewColumn: vscode.ViewColumn.Beside
-				},
-				{
-						enableScripts: true,
-						retainContextWhenHidden: true,
-				}
-		);
+export function startWebview(
+  context: vscode.ExtensionContext,
+  projectName: string,
+  previewPage: string
+): void {
+  // Choose template. Guarantees the correct fonts are loaded in as well
+  const template = projectName === "guides" ? "guides.css" : "mongodb-docs.css";
+  // Create panel
+  const panel = vscode.window.createWebviewPanel(
+    "snootyPreview",
+    `Snooty Preview - ${previewPage}`,
+    {
+      preserveFocus: true,
+      viewColumn: vscode.ViewColumn.Beside
+    },
+    {
+      enableScripts: true,
+      retainContextWhenHidden: true
+    }
+  );
 
-		// Set up resource files needed for page to render (bundle and css)
-		// If file does not exist, panel will show empty content
-		const bundlePath = path.join(
-				context.extensionPath, 
-				'snooty-frontend/preview',
-				'bundle.js'
-		);
-		const bundleFile = getResource(bundlePath);
-		// TODO: Add validation through parser to see which css file to use. 
-		// For testing purposes, only support guides until we get communication between extension and frontend working
-		const cssPath = path.join(
-				context.extensionPath, 
-				'snooty-frontend/static/docs-tools',
-				template
-		);
-		const cssFile = getResource(cssPath);
-		
-		// Assign html content to webview panel
-		panel.webview.html = getWebviewContent(bundleFile, cssFile);
+  // Set up resource files needed for page to render (bundle and css)
+  // If file does not exist, panel will show empty content
+  const bundlePath = path.join(
+    context.extensionPath,
+    "snooty-frontend/preview",
+    "bundle.js"
+  );
+  const bundleFile = getResource(bundlePath);
+  // TODO: Add validation through parser to see which css file to use.
+  // For testing purposes, only support guides until we get communication between extension and frontend working
+  const cssPath = path.join(
+    context.extensionPath,
+    "snooty-frontend/static/docs-tools",
+    template
+  );
+  const cssFile = getResource(cssPath);
+
+  // Assign html content to webview panel
+  panel.webview.html = getWebviewContent(bundleFile, cssFile);
 }
 
 // Takes a path to a file and converts it to a secure resource for webview
 function getResource(path: string): vscode.Uri {
-		const fileUri = vscode.Uri.file(path);
-		return fileUri.with({scheme: 'vscode-resource'});
+  const fileUri = vscode.Uri.file(path);
+  return fileUri.with({ scheme: "vscode-resource" });
 }
 
 // Returns html template used to render preview
 function getWebviewContent(bundleScript: vscode.Uri, css: vscode.Uri): string {
-		return `
-				<!DOCTYPE html>
-				<html lang="en">
-				<head>
-						<meta charSet="utf-8" />
-						<meta httpEquiv="x-ua-compatible" content="ie=edge" />
-						<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-						<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-						<meta name="robots" content="index" />
-						<meta name="release" content="1.0" />
-						<meta name="version" content="master" />
-						<meta name="DC.Source" content="https://github.com/mongodb/docs-bi-connector/blob/DOCSP-3279/source/index.txt" />
-						<meta
-								property="og:image"
-								content="http://s3.amazonaws.com/info-mongodb-com/_com_assets/cms/mongodb-for-giant-ideas-bbab5c3cf8.png"
-						/>
-						<meta
-								property="og:image:secure_url"
-								content="https://webassets.mongodb.com/_com_assets/cms/mongodb-for-giant-ideas-bbab5c3cf8.png"
-						/>
-						<meta
-								http-equiv="Content-Security-Policy"
-								content="default-src 'none'; 
-								img-src vscode-resource: https: data:; 
-								script-src 'unsafe-eval' vscode-resource: data:; 
-								style-src 'unsafe-inline' vscode-resource: https:;
-								font-src https: vscode-resource:"
-						/>
-						<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css" />
-						<link rel="shortcut icon" href="https://media.mongodb.org/favicon.ico" />
-						<link
-								rel="search"
-								type="application/opensearchdescription+xml"
-								href="https://docs.mongodb.com/osd.xml"
-								title="MongoDB Help"
-						/>
-						<link href="${css}" rel="stylesheet" type="text/css"/>
-						<style>
-								body {
-										background: white
-								}
-						</style>
-				</head>
-				<body>
-						<div id="app"></div>
-						<script src="${bundleScript}"></script>
-				</body>
-				</html>`;
+  return `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charSet="utf-8" />
+		<meta httpEquiv="x-ua-compatible" content="ie=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="robots" content="index" />
+		<meta name="release" content="1.0" />
+		<meta name="version" content="master" />
+		<meta name="DC.Source" content="https://github.com/mongodb/docs-bi-connector/blob/DOCSP-3279/source/index.txt" />
+		<meta
+			property="og:image"
+			content="http://s3.amazonaws.com/info-mongodb-com/_com_assets/cms/mongodb-for-giant-ideas-bbab5c3cf8.png"
+		/>
+		<meta
+			property="og:image:secure_url"
+			content="https://webassets.mongodb.com/_com_assets/cms/mongodb-for-giant-ideas-bbab5c3cf8.png"
+		/>
+		<meta
+			http-equiv="Content-Security-Policy"
+			content="default-src 'none'; 
+			img-src vscode-resource: https: data:; 
+			script-src 'unsafe-eval' vscode-resource: data:; 
+			style-src 'unsafe-inline' vscode-resource: https:;
+			font-src https: vscode-resource:"
+		/>
+		<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css" />
+		<link rel="shortcut icon" href="https://media.mongodb.org/favicon.ico" />
+		<link
+			rel="search"
+			type="application/opensearchdescription+xml"
+			href="https://docs.mongodb.com/osd.xml"
+			title="MongoDB Help"
+		/>
+		<link href="${css}" rel="stylesheet" type="text/css"/>
+		<style>
+			body {
+			background: white
+			}
+		</style>
+	</head>
+	<body>
+		<div id="app"></div>
+		<script src="${bundleScript}"></script>
+	</body>
+	</html>`;
 }
